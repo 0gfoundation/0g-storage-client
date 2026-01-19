0167a5eb2e8a79b2a2132f6991e898c6ec3b12dd
Peter Zhang <peter@0g.ai>
2026-01-19 23:19:29 +0800

    refactor test cases

--- tests/client_utility/build_binary.py
+++ tests/client_utility/build_binary.py
@@ -1,7 +1,10 @@
 from enum import Enum, unique
+import os
 
 from utility.utils import is_windows_platform
-from utility.build_binary import __build_from_github
+from utility.build_binary import __asset_name, __build_from_github, __download_from_github
+
+GITHUB_DOWNLOAD_URL = "https://api.github.com/repos/0gfoundation/0g-storage-node/releases/276222268"
 
 ZGS_BINARY = "zgs_node.exe" if is_windows_platform() else "zgs_node"
 KV_BINARY = "zgs_kv.exe" if is_windows_platform() else "zgs_kv"
@@ -14,6 +17,15 @@ class BuildBinaryResult(Enum):
 
 
 def build_zgs(dir: str) -> BuildBinaryResult:
+    result = __download_from_github(
+        dir=dir,
+        binary_name=ZGS_BINARY,
+        github_url=GITHUB_DOWNLOAD_URL,
+        asset_name=__asset_name(ZGS_BINARY, zip=True),
+    )
+    if result != BuildBinaryResult.NotInstalled:
+        return result
+    
     return __build_from_github(
         dir=dir,
         binary_name=ZGS_BINARY,
@@ -24,6 +36,15 @@ def build_zgs(dir: str) -> BuildBinaryResult:
 
 
 def build_kv(dir: str) -> BuildBinaryResult:
+    result = __download_from_github(
+        dir=dir,
+        binary_name=KV_BINARY,
+        github_url=GITHUB_DOWNLOAD_URL,
+        asset_name=__asset_name(KV_BINARY, zip=True),
+    )
+    if result != BuildBinaryResult.NotInstalled:
+        return result
+    
     return __build_from_github(
         dir=dir,
         binary_name=KV_BINARY,
